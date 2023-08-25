fcd9a0fcdced5ea0947901051c1f29badd989953
Vitaly Park <126284852+gitvitaly@users.noreply.github.com>
2023-08-25 12:28:55 -0700

    Update itemsInterface.ts

--- src/interface/itemsInterface.ts
+++ src/interface/itemsInterface.ts
@@ -1,1 +1,12 @@
-export default interface IItems {    id: number    title: string    price: number    description: string    category: string    image: string    rating: {        rate: number        count: number    }}
+export default interface IItems {
+    id: number
+    title: string
+    price: number
+    description: string
+    category: string
+    image: string
+    rating: {
+        rate: number
+        count: number
+    }
+}
